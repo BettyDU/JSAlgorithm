82485fe5be11f02d7435ba90ca71281ba68a53af
BettyDU <604314292@qq.com>
2017-05-19 21:47:52 +0800

    sort

--- sort/sort.js
+++ sort/sort.js
@@ -1,17 +1,17 @@
 function ArrayList() {
-	var array = []; //{1}
-	this.insert = function(item) { //{2}
+	var array = []; 
+	this.insert = function(item) { 
 		array.push(item);
 	};
-	this.toString = function() { //{3}
+	this.toString = function() { 
 		return array.join();
 	};
 	this.bubbleSort = function() {
-		var length = array.length; //{1}
+		var length = array.length; 
 		for (var i = 0; i < length; i++) {
-			for (var j = 0; j < length - 1; j++) { //{3}
-				if (array[j] > array[j + 1]) { //{4}
-					swap(j, j + 1, array); //{5}
+			for (var j = 0; j < length - 1; j++) { 
+				if (array[j] > array[j + 1]) { 
+					swap(j, j + 1, array); 
 				}
 			}
 		}
@@ -20,7 +20,7 @@ function ArrayList() {
 	this.modifiedBubbleSort = function() {
 		var length = array.length;
 		for (var i = 0; i < length; i++) {
-			for (var j = 0; j < length - 1 - i; j++) { //{1}
+			for (var j = 0; j < length - 1 - i; j++) { 
 				if (array[j] > array[j + 1]) {
 					swap(j, j + 1, array);
 				}
@@ -33,13 +33,13 @@ function ArrayList() {
 			indexMin;
 		for (var i = 0; i < length - 1; i++) {
 			indexMin = i;
-			for (var j = i; j < length; j++) {}
-		}
-		if (array[indexMin] > array[j]) { //{5}
-			indexMin = j;
-			//{6} //{7}
-			if (i !== indexMin) {
-				swap(i, indexMin);
+			for (var j = i; j < length; j++) {
+				if (array[indexMin] > array[j]) { 
+					indexMin = j;
+					if (i !== indexMin) {
+						swap(i, indexMin,array);
+					}
+				}
 			}
 		}
 	};
@@ -50,14 +50,14 @@ var swap = function(index1, index2, array) {
 	array[index2] = aux;
 };
 
-function createNonSortedArray(size) { //{6}
+function createNonSortedArray(size) { 
 	var array = new ArrayList();
 	for (var i = size; i > 0; i--) {
 		array.insert(i);
 	}
 	return array;
 }
-var myArray = createNonSortedArray(5); //{7}
+var myArray = createNonSortedArray(5); 
 console.log(myArray.toString());
 myArray.modifiedBubbleSort();
 console.log(myArray.toString());
