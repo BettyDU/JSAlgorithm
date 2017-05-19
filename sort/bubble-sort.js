0310d0f6aa1535c638a7f81d0ca9a727409d887b
BettyDU <604314292@qq.com>
2017-05-19 16:25:40 +0800

    complete bubble

--- sort/bubble-sort.js
+++ sort/bubble-sort.js
@@ -6,19 +6,28 @@ function ArrayList() {
 	this.toString = function() { //{3}
 		return array.join();
 	};
-
 	this.bubbleSort = function() {
 		var length = array.length; //{1}
-		for (var i = 0; i < length; i++) { //{2}
+		for (var i = 0; i < length; i++) {
+			for (var j = 0; j < length - 1; j++) { //{3}
+				if (array[j] > array[j + 1]) { //{4}
+					swap(j, j + 1, array); //{5}
+				}
+			}
 		}
 	};
-	for (var j = 0; j < length - 1; j++) { //{3}
-		if (array[j] > array[j + 1]) { //{4}
-			swap(j, j + 1, array); //{5}
-		}
-	}
+	//改进版的冒泡排序,时间复杂度是O(n2)
+	this.modifiedBubbleSort = function() {
+		var length = array.length;
+		for (var i = 0; i < length; i++) {
+			for (var j = 0; j < length - 1 - i; j++) { //{1}
+				if (array[j] > array[j + 1]) {
+					swap(j, j + 1, array);
+				}
+			}
+		};
+	};
 }
-
 var swap = function(index1, index2, array) {
 	var aux = array[index1];
 	array[index1] = array[index2];
@@ -34,7 +43,5 @@ function createNonSortedArray(size) { //{6}
 }
 var myArray = createNonSortedArray(5); //{7}
 console.log(myArray.toString());
-myArray.bubbleSort();
+myArray.modifiedBubbleSort();
 console.log(myArray.toString());
-//{8}
-//{9} 7 //{10}
