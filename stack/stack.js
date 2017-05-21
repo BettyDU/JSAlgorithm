24516c8c6ab0099f66b46a86d6768131ebe7999e
BettyDU <604314292@qq.com>
2017-05-21 10:06:30 +0800

    添加十进制转换为二进制

--- stack/stack.js
+++ stack/stack.js
@@ -40,3 +40,22 @@ console.log(stack.isEmpty());
 stack.print();
 
 /***********十进制转换为二进制***********/
+function devideBy2(decNumber) {
+	var remStack = new Stack(),
+		rem,
+		binaryString = '';
+
+	while (decNumber > 0) {
+		rem = Math.floor(decNumber % 2); //余数取整
+		remStack.push(rem); //余数入栈
+		decNumber = Math.floor(decNumber / 2); //商取整
+	}
+	//依次取出余数，直到栈为空
+	while (!remStack.isEmpty()) {
+		binaryString += remStack.pop().toString();
+	}
+	return binaryString;
+}
+console.log(divideBy2(233)); //输出11101001
+console.log(divideBy2(10)); //输出1010
+console.log(divideBy2(1000)); //输出1111101000
